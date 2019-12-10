75cabbee370aaa419fdc9a1c7d5956d67828be4b
angela <angelnewa101@gmail.com>
2019-12-10 23:45:21 +0545

    Simple interest

--- app/src/main/java/com/app/fragassignment/fragments/SimpleInterest.java
+++ app/src/main/java/com/app/fragassignment/fragments/SimpleInterest.java
@@ -8,25 +8,44 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.app.fragassignment.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class SimpleInterest extends Fragment {
 
-
-    public SimpleInterest() {
-        // Required empty public constructor
-    }
+public class SimpleInterest extends Fragment implements View.OnClickListener {
+    private Button btnSI;
+    private EditText etPrincipal, etTime,etRate;
+    private TextView tvSI;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simple_interest, container, false);
+        View view=inflater.inflate(R.layout.fragment_simple_interest,container,false);
+        etPrincipal=view.findViewById(R.id.etPrincipal);
+        etRate=view.findViewById(R.id.etRate);
+        etTime=view.findViewById(R.id.etTime);
+        btnSI=view.findViewById(R.id.btnSI);
+        tvSI=view.findViewById(R.id.tvSI);
+
+        btnSI.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        float p,t,r, si;
+        p=Float.parseFloat(etPrincipal.getText().toString());
+        t=Float.parseFloat(etTime.getText().toString());
+        r=Float.parseFloat(etRate.getText().toString());
+
+        si=(p*t*r)/100;
+
+        tvSI.setText("Simple Interest is "+si);
+
+    }
 }
